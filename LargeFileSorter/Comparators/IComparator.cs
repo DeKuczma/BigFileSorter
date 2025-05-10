dc05fc15afa6b650d37bcc52eb63ffd54b6951c5
DeKuczma <dawid.j.kuczma@gmail.com>
2025-05-10 19:45:05 +0200

    Added cComparator interface

---
+++ LargeFileSorter/Comparators/IComparator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargeFileSorter.Comparators
+{
+    public interface IComparator
+    {
+        //return same values as string.ComapreTo
+        public int Compare(StringWriter first, StringWriter second);
+    }
+}
---
+++ LargeFileSorter/Models/Line.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargeFileSorter.Models
+{
+    public class Line
+    {
+        private const string SEPARATOR = ". ";
+        public int Number { get; init; }
+        public string Text { get; init; }
+
+        public Line(string inputLine)
+        {
+            string[] values = inputLine.Split(". ", 2);
+            Number = int.Parse(values[0]);
+            Text = values[1];
+        }
+
+        public int Compare(Line line)
+        {
+            int comaprisonResult = Text.CompareTo(line.Text);
+            if (comaprisonResult != 0)
+            {
+                return comaprisonResult;
+            }
+
+            if(Number < line.Number)
+            {
+                return -1;
+            }
+
+            return 1;
+        }
+
+        public override string ToString()
+        {
+            return $"{Number}{SEPARATOR}{Text}";
+        }
+    }
+}
