0c69c124bf989c959dc4cee130c25ae3f31e98a2
DeKuczma <dawid.j.kuczma@gmail.com>
2025-05-11 11:23:32 +0200

    Added stopwatch to measure runtime

--- LargeFileSorter/Program.cs
+++ LargeFileSorter/Program.cs
@@ -1,6 +1,13 @@
-﻿using LargeFileSorter.Comparators;
-using LargeFileSorter.FileMerge;
+﻿using LargeFileSorter.FileMerge;
+using System.Diagnostics;
+
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
 
 IFileMerge fileMerge = new MemoryEfficientFileMerge();
 
 fileMerge.MergeFiles("test1.txt", "test2.txt", "result.txt");
+
+stopwatch.Stop();
+Console.WriteLine($"File sorted in: {stopwatch.Elapsed}");
