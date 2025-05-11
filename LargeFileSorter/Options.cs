cf0150ce978fa9c1fbf37d5698fc39d23aea8f72
DeKuczma <dawid.j.kuczma@gmail.com>
2025-05-11 12:37:40 +0200

    Memory Efficient Merge fix, Added parsing input parameters

---
+++ LargeFileSorter/Options.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace LargeFileSorter
+{
+    public class Options
+    {
+        [Option('i', "input", Required = false, HelpText = "Input file path.")]
+        public string InputFile { get; set; }
+
+        [Option('o', "output", Required = false, HelpText = "Output file path.")]
+        public string OutputFile { get; set; }
+    }
+}
