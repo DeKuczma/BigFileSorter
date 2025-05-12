aebaf4292847711fd30d4c854537990f97755478
DeKuczma <dawid.j.kuczma@gmail.com>
2025-05-12 10:04:39 +0200

    Change options settings to required

--- LargeFileSorter/Options.cs
+++ LargeFileSorter/Options.cs
@@ -4,10 +4,10 @@ namespace LargeFileSorter
 {
     public class Options
     {
-        [Option('i', "input", Required = false, HelpText = "Input file path.")]
+        [Option('i', "input", Required = true, HelpText = "Input file path.")]
         public string InputFile { get; set; }
 
-        [Option('o', "output", Required = false, HelpText = "Output file path.")]
+        [Option('o', "output", Required = true, HelpText = "Output file path.")]
         public string OutputFile { get; set; }
 
         public static int WriterBuferSize { get; } = 1024 * 64;
